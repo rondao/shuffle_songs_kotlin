bb1fc0b09dfa259b1b6fe7cd3876f1c27e86aa5a
Rafael Rondão <rafael.rondao@gmail.com>
2019-12-14 18:54:52 -0300

    Refactor splash to a fragment with navigation
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

--- app/src/main/java/com/rondao/shufflesongs/MainActivity.kt
+++ app/src/main/java/com/rondao/shufflesongs/MainActivity.kt
@@ -3,11 +3,10 @@ package com.rondao.shufflesongs
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-
-class SplashActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_main)
     }
 }
