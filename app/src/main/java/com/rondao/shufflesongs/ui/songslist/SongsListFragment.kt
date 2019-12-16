62de80ef5ebc75a7d6d4e2c60866865946063ea0
Rafael Rondão <rafael.rondao@gmail.com>
2019-12-15 22:45:29 -0300

    Add divider decoration to recycler view
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

--- app/src/main/java/com/rondao/shufflesongs/ui/songslist/SongsListFragment.kt
+++ app/src/main/java/com/rondao/shufflesongs/ui/songslist/SongsListFragment.kt
@@ -1,14 +1,17 @@
 package com.rondao.shufflesongs.ui.songslist
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import com.rondao.shufflesongs.R
 import com.rondao.shufflesongs.databinding.FragmentSongsListBinding
+
 
 class SongsListFragment : Fragment() {
 
@@ -22,6 +25,10 @@ class SongsListFragment : Fragment() {
 
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+
+        val divider = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        getDrawable(context!!, R.drawable.divider)?.let { divider.setDrawable(it) }
+        binding.songsList.addItemDecoration(divider)
 
         val adapter = SongsListAdapter()
         binding.songsList.adapter = adapter
