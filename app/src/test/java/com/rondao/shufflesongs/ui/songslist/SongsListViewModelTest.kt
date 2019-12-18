b4d0f386967100f96656bc6fd9b3fd2275f207a9
Rafael Rondão <rafael.rondao@gmail.com>
2019-12-18 12:24:06 -0300

    Refactor shuffled songs test with Before
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

--- app/src/test/java/com/rondao/shufflesongs/ui/songslist/SongsListViewModelTest.kt
+++ app/src/test/java/com/rondao/shufflesongs/ui/songslist/SongsListViewModelTest.kt
@@ -6,12 +6,13 @@ import com.rondao.shufflesongs.getOrAwaitValue
 import com.rondao.shufflesongs.network.WrapperType.Track
 import org.hamcrest.CoreMatchers.*
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 import org.junit.Rule
 
 class SongsListViewModelTest {
+    // Generate fake data for tests
     private val songsByArtist: Map<Int, List<Track>>
-
     init {
         val map = mutableMapOf<Int, List<Track>>()
         val size = 5
@@ -27,25 +28,32 @@ class SongsListViewModelTest {
         songsByArtist = map
     }
 
+    private lateinit var songsListViewModel: SongsListViewModel
+    private lateinit var shuffledSongsList: List<Track>
+
+    @Before
+    fun setupViewModel() {
+        songsListViewModel = SongsListViewModel()
+        songsListViewModel.shuffleSongs(songsByArtist)
+
+        shuffledSongsList = songsListViewModel.songsList.getOrAwaitValue()
+    }
+
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
 
     @Test
-    fun shuffleSongs_standardSongsList_noAdjacent() {
-        val songsListViewModel = SongsListViewModel()
-
-        songsListViewModel.shuffleSongs(songsByArtist)
-        val shuffledSongsList = songsListViewModel.songsList.getOrAwaitValue()
-
-        // Every song should be at shuffled list
+    fun shuffleSongs_standardSongsList_containsEverySong() {
         songsByArtist.forEach { (_, songsList) ->
             songsList.forEach {
                 assertThat(shuffledSongsList, hasItem(it))
             }
         }
+    }
 
-        // There shouldn't be adjacent songs by same artist
-        var previousSong = shuffledSongsList!![0]
+    @Test
+    fun shuffleSongs_standardSongsList_noAdjacent() {
+        var previousSong = shuffledSongsList[0]
         shuffledSongsList.drop(1).forEach { currentSong ->
             assertThat(previousSong.artistId, not(equalTo(currentSong.artistId)))
             previousSong = currentSong
@@ -54,24 +62,12 @@ class SongsListViewModelTest {
 
     @Test
     fun shuffleSongs_nullOrEmptySongsList_noChange() {
-        val songsListViewModel = SongsListViewModel()
-
-        songsListViewModel.shuffleSongs(songsByArtist)
-        val shuffledSongsList = songsListViewModel.songsList.getOrAwaitValue()
-
-        // Every song should be at shuffled list
-        songsByArtist.forEach { (_, songsList) ->
-            songsList.forEach {
-                assertThat(shuffledSongsList, hasItem(it))
-            }
-        }
-
         songsListViewModel.shuffleSongs(null)
         val shuffledSongsListAfterNull = songsListViewModel.songsList.getOrAwaitValue()
         assertThat(shuffledSongsList, equalTo(shuffledSongsListAfterNull))
 
         songsListViewModel.shuffleSongs(mapOf())
         val shuffledSongsListAfterEmpty = songsListViewModel.songsList.getOrAwaitValue()
-        assertThat(shuffledSongsList, equalTo(shuffledSongsListAfterNull))
+        assertThat(shuffledSongsList, equalTo(shuffledSongsListAfterEmpty))
     }
 }
