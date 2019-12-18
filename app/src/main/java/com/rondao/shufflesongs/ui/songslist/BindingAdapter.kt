274eacb5ffd33fbda0b401c2a5966ff6bf903031
Rafael Rondão <rafael.rondao@gmail.com>
2019-12-18 15:59:10 -0300

    Format artists name and genre from Track
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

--- app/src/main/java/com/rondao/shufflesongs/ui/songslist/BindingAdapter.kt
+++ app/src/main/java/com/rondao/shufflesongs/ui/songslist/BindingAdapter.kt
@@ -2,12 +2,14 @@ package com.rondao.shufflesongs.ui.songslist
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.core.widget.ContentLoadingProgressBar
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.rondao.shufflesongs.R
+import com.rondao.shufflesongs.network.WrapperType.Track
 
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
@@ -21,6 +23,11 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
                         .error(R.drawable.ic_baseline_broken_image_24))
                 .into(imgView)
     }
+}
+
+@BindingAdapter("formatArtistGenre")
+fun bindFormatSongTitle(txtView: TextView, song: Track) {
+    txtView.text = "${song.artistName} (${song.primaryGenreName})"
 }
 
 @BindingAdapter("songsApiStatusLoading")
